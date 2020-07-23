900f13e4457c5e7df4dd233e122695b1eb8b94ff
diskii <diskii100@gmail.com>
2020-07-23 22:15:03 +0900

    deploying latest code to develop

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# todo: create a script for deployment
-# automate the entire process
-# first build the program,
-# then after building deploy the program to the master branch and prompt the user, yes or no
 echo 'Script for deploying code to gh-pages'
 npm run build
 echo 'Dist file building is now complete'
 while true; do
-    read -p "Would you like to deploy the code to the gh-pages branch" yn
+    read -p "Would you like to deploy the code to the gh-pages branch\n" yn
     case $yn in
-        [Yy]* ) git subtree push --prefix dist origin gh-pages; break;;
+        [Yy]* )
+          git add -A
+          git commit -m 'deploying latest code to develop'
+          git push
+          git subtree push --prefix dist origin gh-pages;
+          break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
